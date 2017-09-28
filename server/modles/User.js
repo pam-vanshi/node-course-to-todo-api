00b455edda0b50197c5ab7ac84c3fedb4e1dea8b
Pramudit <spramudit@gmail.com>
2017-09-28 07:58:32 +0530

    bjgig

--- server/modles/User.js
+++ server/modles/User.js
@@ -1,10 +1,13 @@
 
 const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   name: {
   type: String,
-  required: true,
+  //required: true,
   minlength: 1,
   default: null,
   trim: true
@@ -13,10 +16,77 @@ var User = mongoose.model('User', {
     type: String,
     trim: true,
     minlength: 5,
-    default: "user@user.com"
+    unique: true,
+    required: true,
+    validate: {
+      validator: validator.isEmail,
+      message: `{VALUE} is not a valid email`
+    }
   },
+  password: {
+        type: String,
+        //required: true,
+        minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type:String,
+      required: true
+    }
+  }]
 
 })
+
+UserSchema.methods.toJSON = function() {
+  var user = this;
+  var userObject = user.toObject()
+
+  return _.pick(userObject,['name','email','_id'])
+}
+
+
+
+UserSchema.methods.generateAuthToken = function() {
+  var user = this;
+  var access = 'auth'
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+  user.tokens.push({
+    access: access,
+    token: token
+  })
+  return user.save().then(() => {
+    return token;
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var User = mongoose.model('User', UserSchema)
 
 module.exports = {
   User
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
@@ -62,6 +63,45 @@ app.delete('/todos/:id', (req, res) => {
   })
 })
 
+app.patch('/user/:id', (req,res) => {
+  var id = req.params.id;
+  var body = _.pick('req.body', ['text','completed'])
+  if(!ObjectID.isValid(id)){
+    return res.status(400).send("Id is not valid")
+  }
+  if(_.isBoolean(body.completed) & body.completed){
+    body.completedAt = new Date().getTime()
+  }
+  else{
+    body.completed = false,
+    body.completedAt = null
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){res.send('there is no todo')}
+    res.send(todo)
+  }, (err) => {
+    res.status(400).send()
+  })
+
+})
+
+
+app.post('/user', (req,res) => {
+  var body = _.pick(req.body, ['name','email','password'])
+  var user = new User({
+    name: body.name,
+    email: body.email,
+    password: body.password
+
+  })
+  user.save().then((user) => {
+     return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth',token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
+  })
+})
 
 
 
